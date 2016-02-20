3afd29a36f3c16b8fd5f3e23ae6fa4cf1ad21d4e
ball <ballz_v@hotmail.com>
2016-02-20 14:02:34 +0700

    Explicit เสร็จแล้ว

--- app/src/main/java/com/su/lapponampai_w/mytraffic/MainActivity.java
+++ app/src/main/java/com/su/lapponampai_w/mytraffic/MainActivity.java
@@ -2,12 +2,23 @@ package com.su.lapponampai_w.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ประกาศตัวแปร...... Explicit บอก Device ให้พร้อมทั้งเครื่องและ Ram
+    private ListView trafficListView;
+    private Button aboutButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+    }    //Main Method
+
+}   //Main Class
